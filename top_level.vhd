76d69692d0ad089b644b4841cf1cd0578375483a
K-Mozaffari <kamal_mozaffarie@yahoo.com>
2021-08-16 02:19:58 -0400

    year,month,day and setting option

--- top_level.vhd
+++ top_level.vhd
@@ -33,7 +33,10 @@ ENTITY top_level IS
         rw, rs, e       : OUT   STD_LOGIC;  --read/write, setup/data, and enable for lcd
         lcd_data        : OUT   STD_LOGIC_VECTOR(7 DOWNTO 0);
         constrastup     :in     std_logic;
-        constrastdown   :in     std_logic;
+        s_min   :in     std_logic;
+		  s_hour	   :in     std_logic;
+		  s_month:in std_logic;
+		  s_year:in std_logic;
 	    led             :out    std_logic	;
         leddbkey        :out    std_logic
 			); --data signals for lcd
@@ -90,18 +93,17 @@ Port (
 end COMPONENT;
 
 
-signal s0 :std_logic_vector(3 downto 0):=(others=>'0');
-signal s1 :std_logic_vector(3 downto 0):=(others=>'0');
-signal m0 :std_logic_vector(3 downto 0):="1001";
-signal m1 :std_logic_vector(3 downto 0):="0101";
-signal h0 :std_logic_vector(3 downto 0):="0010";
-signal h1 :std_logic_vector(3 downto 0):="0001";
-signal clk:std_logic;
-signal pm,am: std_logic_vector(19 downto 0):=(others=>'0');
-signal apm: std_logic_vector(19 downto 0):="10011000011001101101";
-signal months:std_logic_vector(71 downto 0):="010010100110000101101110011101010110000101110010011110011010000010100000";
-signal month:integer range 1 to 12 :=1;
-signal year: std_logic_vector(15 downto 0):="0011001000110001";
+signal sec		:	integer range 0 to 59 :=0;
+signal min		:	integer range 0 to 59 :=0;
+signal hour		:	integer range 1 to 12:=1;
+signal day		:	integer range 1 to 31 :=1;
+signal month	:	integer range 1 to 12 :=1;
+signal year		:	integer range 20 to 50 :=20;
+signal clk		:	std_logic;
+signal pm,am	:	std_logic_vector(19 downto 0):=(others=>'0');
+signal apm		:	std_logic_vector(19 downto 0):="10011000011001101101";
+signal months	:	std_logic_vector(23 downto 0):="010010100110000101101110";
+signal set		:	std_logic_vector(3 downto 0):=(others=>'0');
 -------------------------------------------------------------------
 BEGIN
 
@@ -139,6 +141,7 @@ gcontraxt: PWM  Generic map(
   PROCESS(clk)
     VARIABLE char  :  INTEGER RANGE 0 TO 50000000 := 0;
   BEGIN
+ 
     IF(clk'EVENT AND clk = '1') THEN
       IF(lcd_busy = '0' AND lcd_enable = '0') THEN
 	  
@@ -149,33 +152,31 @@ gcontraxt: PWM  Generic map(
           char := char + 1;
         END IF;
         CASE char IS
-	       WHEN 1 => lcd_bus <= "100011"&h1;
-          WHEN 2 => lcd_bus <= "100011"&h0;
+	       WHEN 1 => lcd_bus <= "100011"&conv_std_logic_vector(hour/10,4);--conv_std_logic_vector(input_1, output_1'length)
+          WHEN 2 => lcd_bus <= "100011"&conv_std_logic_vector(hour mod 10,4);
           WHEN 3 => lcd_bus <= "100011"&"1010";	  
-          WHEN 4 => lcd_bus <= "100011"&m1;
-          WHEN 5 => lcd_bus <= "100011"&m0;
+          WHEN 4 => lcd_bus <= "100011"&conv_std_logic_vector(min/10,4);
+          WHEN 5 => lcd_bus <= "100011"&conv_std_logic_vector(min mod 10,4);
           WHEN 6 => lcd_bus <= "100011"&"1010";
-          WHEN 7 => lcd_bus <= "100011"&s1;
-          WHEN 8 => lcd_bus <= "100011"&s0;
+          WHEN 7 => lcd_bus <= "100011"&conv_std_logic_vector(sec/10,4);
+          WHEN 8 => lcd_bus <= "100011"&conv_std_logic_vector(sec mod 10,4) ;
           WHEN 9 => lcd_bus <= "101010"&"0000";
           WHEN 10 => lcd_bus <= apm(19 downto 10);			 
           WHEN 11 => lcd_bus <= apm(9 downto 0);
 			 when 12 => lcd_bus<=  "0011000000";
-			 when 13 =>lcd_bus<="10"&months(71 downto 64);
-			 when 14 =>lcd_bus<="10"&months(63 downto 56);
-			 when 15 =>lcd_bus<="10"&months(55 downto 48);
-			 when 16 =>lcd_bus<="10"&months(47 downto 40);
-			 when 17 =>lcd_bus<="10"&months(39 downto 32);
-			 when 18 =>lcd_bus<="10"&months(31 downto 24);
-			 when 19 =>lcd_bus<="10"&months(23 downto 16);
-			 when 20 =>lcd_bus<="10"&months(15 downto 8);
-			 when 21 =>lcd_bus<="10"&months(7 downto 0);
-			 when 22 =>lcd_bus<="1010100000";
-			 when 23 =>lcd_bus<="1000110010";
-			 when 24 =>lcd_bus<="1000110000";
-			 when 25 =>lcd_bus<="10"&year(15 downto 8);
-			 when 26 =>lcd_bus<="10"&year(7 downto 0);
-			 when 27 => lcd_bus<=  "0000000010";
+			 when 13 =>lcd_bus<="10"&months(23 downto 16);
+			 when 14 =>lcd_bus<="10"&months(15 downto 8);
+			 when 15 =>lcd_bus<="10"&months(7 downto 0);
+			 WHEN 16 => lcd_bus <= "1010100000";
+			 WHEN 17 => lcd_bus <= "1010100000";
+			 when 18 =>lcd_bus<="100011"&conv_std_logic_vector(day/10,4);
+			 when 19=>lcd_bus<="100011"&conv_std_logic_vector(day mod 10,4) ;
+			 when 20 =>lcd_bus<="1010100000";
+			 when 21 =>lcd_bus<="1000110010";
+			 when 22 =>lcd_bus<="1000110000";
+			 when 23 =>lcd_bus<="100011"&conv_std_logic_vector(year/10,4);
+			 when 24 =>lcd_bus<="100011"&conv_std_logic_vector(year mod 10,4) ;
+			 when 25 => lcd_bus<=  "0000000010";
 			 when 50000000=>char:=0;
           WHEN OTHERS => lcd_enable <= '0'; 
         END CASE;
@@ -183,199 +184,165 @@ gcontraxt: PWM  Generic map(
         lcd_enable <= '0';
       END IF;
     END IF;
+	 
   END PROCESS;
-process(clk)
+  
+  set<=s_year&s_month&s_hour&s_min;
+process(clk,set)
 variable char: integer range 0 to 50000000 :=0;
 begin 
-if rising_edge(clk) then ---1
-	if char<50000000 then ---2
-		char:=char+1;
-	elsif char=50000000 then ---3
-			char:=0;
-			if s0<9 then 
-				s0<=s0+1;
+	
+		if rising_edge(clk) then ---1
+			if char<50000000 then ---2
+				char:=char+1;
 			else
-				s0<=(others=>'0');
-				if s1<5 then 
-					s1<=s1+1;
-				else 
-					s1<=(others=>'0');
-					if m0< 9 then 
-						m0<=m0+1;
-					else 
-						m0<=(others=>'0');
-						if m1 <5 then
-							m1<=m1+1;
-						else
-							m1<=(others=>'0');
-							if h1=0  then
-									if h0<9 then
-										h0<=h0+1;
-									else 
-										h0<=(others=>'0');
-										h1<=h1+1;
-									end if;
+			char:=0;
+				CASE set IS 
+					WHEN "0000" =>
+							
+							if sec<59  then 
+								sec<=sec+1;
 							else
-								if h0<2 then 
-									h0<=h0+1;
-								else
-									h0<="0001";
-									h1<=(others=>'0');
-									if apm=am then
-										apm<=pm;
-									else
-										apm<=pm;
+								sec<=0;
+								if min<59 then 
+									min<=min+1;
+								else 
+									min<=0;
+									if hour< 12 then 
+										hour<=hour+1;
+									else 
+										hour<=1;
+										if apm=am then 
+											apm<=pm;
+										else 
+											apm <=am;
+											if day <31 then
+												day<=day+1;
+											else
+												day<=1;
+												if month<12  then
+													month<=month+1;
+												else 
+													month<=1;
+													if year<50 then
+														year<=year+1;
+													else 
+														year<=20;
+													end if;
+												end if;
+											end if;
+										end if;
 									end if;
 								end if;
 							end if;
-						end if;
-					end if;
-				end if;
-			end if;
-			
-	end if;
-end if;
+											
+												
+					WHEN "0001" => 
+							if min<59 then 
+								min <=min+1;
+							else 
+								min<=0;
+							end if;
+							
+					WHEN "0010"=> --set 	
+							if hour< 12 then 
+								hour<=hour+1;
+							else 
+								hour<=1;
+								if apm=am then 
+									apm<=pm;
+								else 
+									apm <=am;
+								end if;
+							end if;
+								
+					WHEN "0100" => --set day
+							if day <31 then
+								day<=day+1;
+							else
+								day<=1;
+							end if;
+								
+					WHEN "1000"=>-- set month
+							if month<12 then 
+								month<=month+1;
+							else 
+								month <=1;
+							end if ;
+					WHEN OTHERS=>-- set year
+								if year<50 then
+									year<=year+1;
+								else 
+									year<=20;
+								end if;
+								
 
+				END CASE ;
+				
+			end if;
+		END IF;
 end process;
 
 
 am<="1001100001"&"1001101101";
 pm<="1001110000"&"1001101101";
---January	- 31 days
---February	- 28 days in a common year and 29 days in leap years
---March		- 31 days
---April		- 30 days
---May 		- 31 days
---June		- 30 days
---July		- 31 days
---August		- 31 days
---September - 30 days
---October	- 31 days
---November	- 30 days
---December	- 31 days
+--January	- 31 days1
+--February	- 28 days 2in a common year and 29 days in leap years
+--March		- 31 days3
+--April		- 30 days4
+--May 		- 31 days5
+--June		- 30 days6
+--July		- 31 days7
+--August		- 31 days8
+--September - 30 days9
+--October	- 31 days10
+--November	- 30 days11
+--December	- 31 days12
 
-process (constrastup)
+process (month)
 
 begin 
-if rising_edge(constrastup) then 
-	if month<12 then 
-	month<=month+1;
-	else 
-	month <=1;
-	end if ;
 	case month is 
 		when 1 =>months<="01001010"&--J January
 							  "01100001"&--a
-							  "01101110"&--n
-							  "01110101"&--u
-							  "01100001"&--a
-							  "01110010"&--r
-							  "01111001"&--y
-							  "10100000"&
-							  "10100000";
+							  "01101110";
 		when 2 =>months<="01000110"&--F February
 							  "01100101"&--e
-							  "01100010"&--b
-							  "01110010"&--r
-							  "01110101"&--u
-							  "01100001"&--a
-							  "01110010"&--r
-							  "01111001"&--y
-							  "10100000";							  
+							  "01100010";							  
 							  
 		when 3 =>months<="01001101"&--M
 							  "01100001"&--a
-							  "01110010"&--r
-							  "01100011"&--c
-							  "01101000"&--h
-							  "10100000"&--
-							  "10100000"&--
-							  "10100000"&--
-							  "10100000";--
+							  "01110010";--
 		when 4  =>months<="01000001"&--A April
 							  "01110000"&--p
-							  "01110010"&--r
-							  "01101001"&--i
-							  "01101100"&--l
-							  "10100000"&--
-							  "10100000"&--
-							  "10100000"&--
-							  "10100000";--
+							  "01110010";--r
 		when 5  =>months<="01001101"&-- May
 							  "01100001"&-- a
-							  "01111001"&-- y
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000";
+							  "01111001";--y
 		when 6  =>months<="01001010"&--June
 							  "01110101"&--u
-							  "01101110"&--n
-							  "01100101"&--e
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000";
+							  "01101110";
 		when 7  =>months<="01001010"&--July
 							  "01110101"&--u
-							  "01101100"&--l
-							  "01111001"&--y
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000"&
-							  "10100000";
+							  "01101100";
 		when 8  =>months<="01000001"&--August
 							  "01110101"&--u
-							  "01100111"&--g
-							  "01110101"&--u
-							  "01110011"&--s
-							  "01110100"&--t
-							  "10100000"&
-							  "10100000"&
-							  "10100000";
+							  "01100111";
 		when 9  =>months<="01010011"&-- September
 							  "01100101"&--e
-							  "01110000"&--p
-							  "01110100"&--t
-							  "01100101"&--e
-							  "01101101"&--m
-							  "01100010"&--b
-							  "01100101"&--e
-							  "01110010";	
+							  "01110000";	
 		when 10  =>months<="01001111"&--October
 							  "01100011"&--c
-							  "01110100"&--t
-							  "01101111"&--o
-							  "01100010"&--b
-							  "01100101"&--e
-							  "01110010"&--r
-								"10100000"&
-								"10100000";
+							  "01110100";
 		when 11 =>months<="01001110"&--November
 							  "01101111"&--o
-							  "01110110"&--v
-							  "01100101"&--e
-							  "01101101"&--m
-							  "01100010"&--b
-							  "01100101"&--e
-								"01110010"&--r
-								"10100000";
+							  "01110110";
 		when 12  =>months<="01000100"&--December
 							  "01100101"&--e
-							  "01100011"&--c
-							  "01100101"&--e
-							  "01101101"&--m
-							  "01100010"&--b
-							  "01100101"&--e
-								"01110010"&--r
-								"10100000";
+							  "01100011";
 		when others =>months<=(others=>'0');
 end case;
-end if;
+
 end process;
 
 END behavior;
